f995175365d7e73ecff19cbec3cf39afdb838f6e
Matthieu Petit <p.matthieu@me.com>
2024-01-24 23:01:06 +0100

    feat(home): add pull to refresh hint

---
+++ src/components/Animations/ScrollDownAnimation.tsx
@@ -0,0 +1,33 @@
+import LottieView, { type AnimatedLottieViewProps } from 'lottie-react-native';
+import React, { forwardRef, type ForwardRefRenderFunction, useMemo } from 'react';
+import { useColorScheme } from 'react-native';
+import tw from 'twrnc';
+import ScrollDownEasey from '@/assets/animations/lottieflow/scroll-down-easey.json';
+import { colouriseLottie } from '@/helpers/colors';
+
+type AnimationProps = Omit<AnimatedLottieViewProps, 'source'>;
+
+const ScrollDownAnimation: ForwardRefRenderFunction<LottieView, AnimationProps> = (props, ref) => {
+  const colorScheme = useColorScheme();
+  const colorizedSource = useMemo(() => {
+    const color = (tw.prefixMatch('dark') ? tw.color('gray-400') : tw.color('gray-500')) as string;
+    return colouriseLottie(ScrollDownEasey, {
+      // top 2.arrow.Stroke 1
+      'layers.0.shapes.0.it.1.c.k': color,
+      // Scroll down/top_v6.Group 1.Stroke 1
+      'layers.1.shapes.0.it.1.c.k': color,
+      // Scroll down/top_v5.Group 1.Stroke 1
+      'layers.2.shapes.0.it.1.c.k': color,
+      // Scroll down/top_v3.Group 1.Stroke 1
+      'layers.3.shapes.0.it.1.c.k': color,
+      // Scroll down/top_v2.Group 1.Stroke 1
+      'layers.4.shapes.0.it.1.c.k': color,
+      // top.arrow.Stroke 1
+      'layers.5.shapes.0.it.1.c.k': color,
+    });
+  }, [colorScheme]);
+
+  return <LottieView ref={ref} loop {...props} autoPlay source={colorizedSource} />;
+};
+
+export default forwardRef(ScrollDownAnimation);
---
+++ src/components/Home/PullToRefreshHint.tsx
@@ -0,0 +1,27 @@
+import ScrollDownAnimation from '../Animations/ScrollDownAnimation';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { View } from 'react-native';
+import Animated, { type StyleProps } from 'react-native-reanimated';
+import tw from 'twrnc';
+import useSettingsStore from '@/stores/settings';
+
+const PullToRefreshHint = ({ style }: { style?: StyleProps }) => {
+  const { t } = useTranslation();
+  const hasLearnPullToRefresh = useSettingsStore((state) => state.hasLearnPullToRefresh);
+
+  if (hasLearnPullToRefresh) {
+    return null;
+  }
+
+  return (
+    <View style={[tw`flex flex-col items-center gap-1`, style]}>
+      <ScrollDownAnimation style={tw`h-6`} />
+      <Animated.Text style={tw`text-xs font-extralight text-slate-500 dark:text-slate-400`}>
+        {t('home.refresh.hint')}
+      </Animated.Text>
+    </View>
+  );
+};
+
+export default PullToRefreshHint;
