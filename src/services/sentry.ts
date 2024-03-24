50a60651ff78574a6383b90c0bccb6f52a474419
Matthieu Petit <p.matthieu@me.com>
2024-03-24 13:06:03 +0100

    fix(sentry): enabled in production build

--- src/services/sentry.ts
+++ src/services/sentry.ts
@@ -1,4 +1,5 @@
 import * as Sentry from '@sentry/react-native';
+import Constants, { ExecutionEnvironment } from 'expo-constants';
 
 // Construct a new instrumentation instance. This is needed to communicate between the integration and React
 export const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
@@ -8,12 +9,11 @@ export const initSentry = () => {
     dsn:
       process.env.EXPO_PUBLIC_SENTRY_DSN ||
       'https://c9153d09b7ba476985cac82872da4098@o4505571822862336.ingest.sentry.io/4505571856023552',
-    enabled: !__DEV__,
+    enabled: Constants.executionEnvironment === ExecutionEnvironment.Standalone,
     integrations: [
       new Sentry.ReactNativeTracing({
         // Pass instrumentation to be used as `routingInstrumentation`
         routingInstrumentation,
-        // ...
       }),
     ],
   });
