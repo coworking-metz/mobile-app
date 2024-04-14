3de2678e62092dd5c62ce0c7ade09eecd0a30fc4
Matthieu Petit <p.matthieu@me.com>
2024-04-14 14:42:10 +0200

    feat(settings): add roles chips

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [unreleased]
 
+### Added
+
+- User roles on settings screen.
+
 ### Changed
 
 - Use `voting member` term instead of `active member` for member status.
--- src/components/Home/MembershipBottomSheet.tsx
+++ src/components/Home/MembershipBottomSheet.tsx
@@ -53,7 +53,7 @@ const MembershipBottomSheet = ({
       style={style}
       onClose={onClose}
       {...(Platform.OS === 'android' && { animationConfigs: { duration: 300 } })}>
-      <View style={tw`flex flex-col w-full p-6`}>
+      <View style={tw`flex flex-col w-full px-6 pt-6`}>
         <View style={tw`flex items-center justify-center h-40 overflow-visible`}>
           <MembershipFormAnimation active={active && valid} style={tw`h-56 w-full`} valid={valid} />
         </View>
