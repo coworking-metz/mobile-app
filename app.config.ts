b65e27cd0493b6c3178e4de92c4618f69365f563
Matthieu Petit <p.matthieu@me.com>
2024-05-01 17:49:05 +0200

    chore(dependencies): prepare expo 51

--- app.config.ts
+++ app.config.ts
@@ -17,30 +17,4 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     ...config.extra,
     buildDate: new Date().toISOString(), // https://stackoverflow.com/a/65970202
   },
-  plugins: [
-    'expo-localization',
-    [
-      'expo-build-properties',
-      {
-        android: {
-          minSdkVersion: 21,
-          compileSdkVersion: 34,
-          targetSdkVersion: 34,
-          buildToolsVersion: '34.0.0',
-          kotlinVersion: '1.8.10',
-        },
-        ios: {
-          deploymentTarget: '14.0',
-          useFrameworks: 'static',
-        },
-      },
-    ],
-    [
-      'expo-custom-assets',
-      {
-        // Add asset directory paths, the plugin copies the files in the given paths to the app bundle folder named Assets
-        assetsPaths: ['./src/assets/rive'],
-      },
-    ],
-  ],
 });
