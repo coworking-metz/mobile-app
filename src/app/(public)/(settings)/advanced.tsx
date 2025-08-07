769ef6021640b5feb23b43f26a338fae87fa4496
Matthieu Deleys <p.matthieu@me.com>
2025-08-07 23:05:01 +0200

    feat(advanced): reorganize screen

--- src/app/(public)/(settings)/advanced.tsx
+++ src/app/(public)/(settings)/advanced.tsx
@@ -6,10 +6,11 @@ import * as Updates from 'expo-updates';
 import { isNil } from 'lodash';
 import React, { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Alert, TextInput, View } from 'react-native';
+import { Alert, View } from 'react-native';
 import { Switch } from 'react-native-ui-lib';
 import tw, { useDeviceContext } from 'twrnc';
-import AppText from '@/components/AppText';
+import AppTextField from '@/components/AppTextField';
+import SectionTitle from '@/components/Layout/SectionTitle';
 import ServiceLayout from '@/components/Layout/ServiceLayout';
 import ServiceRow from '@/components/Layout/ServiceRow';
 import { theme } from '@/helpers/colors';
@@ -41,8 +42,8 @@ const Advanced = () => {
     async (value: boolean) => {
       if (value) {
         Alert.alert(
-          t('advanced.store.tokensStorage.alert.title'),
-          t('advanced.store.tokensStorage.alert.message'),
+          t('advanced.actions.switchTokensStorage.alert.title'),
+          t('advanced.actions.switchTokensStorage.alert.message'),
           [
             {
               text: t('actions.cancel'),
@@ -83,7 +84,7 @@ const Advanced = () => {
     ])
       .then(() => {
         toastStore.add({
-          message: t('advanced.store.clearCache.onCleared.success'),
+          message: t('advanced.actions.clearCache.onCleared.success'),
           type: 'success',
           timeout: 3_000,
         });
@@ -91,7 +92,7 @@ const Advanced = () => {
       .catch(async (error: Error) => {
         const description = await parseErrorText(error);
         noticeStore.add({
-          message: t('advanced.store.clearCache.onCleared.fail'),
+          message: t('advanced.actions.clearCache.onCleared.fail'),
           description,
           type: 'error',
         });
@@ -109,7 +110,7 @@ const Advanced = () => {
       )
       .then(() => {
         toastStore.add({
-          message: t('advanced.store.reset.onReset.success'),
+          message: t('advanced.actions.reset.onReset.success'),
           type: 'success',
           timeout: 3000,
         });
@@ -118,7 +119,7 @@ const Advanced = () => {
       .catch(async (error: Error) => {
         const description = await parseErrorText(error);
         noticeStore.add({
-          message: t('advanced.store.reset.onReset.fail'),
+          message: t('advanced.actions.reset.onReset.fail'),
           description,
           type: 'error',
         });
@@ -130,8 +131,8 @@ const Advanced = () => {
 
   const confirmReset = useCallback(() => {
     Alert.alert(
-      t('advanced.store.reset.confirm.title'),
-      t('advanced.store.reset.confirm.message'),
+      t('advanced.actions.reset.confirm.title'),
+      t('advanced.actions.reset.confirm.message'),
       [
         {
           text: t('actions.cancel'),
@@ -155,44 +156,51 @@ const Advanced = () => {
       title={t('advanced.title')}
       withBackButton={!_root}>
       <View style={tw`w-full max-w-xl mx-auto mb-6`}>
-        <AppText style={tw`text-sm uppercase text-slate-500 mx-6`}>
-          {t('advanced.services.title')}
-        </AppText>
+        <SectionTitle style={tw`mx-6`} title={t('advanced.actions.title')} />
+
         <ServiceRow
           withBottomDivider
-          description={HTTP.defaults.baseURL}
-          label={t('advanced.services.apiBaseUrl.label')}
-          renderDescription={() => (
-            <TextInput
-              autoCapitalize="none"
-              keyboardType="url"
-              placeholder={HTTP.defaults.baseURL}
-              placeholderTextColor={
-                tw.prefixMatch('dark') ? tw.color('slate-400/50') : tw.color('slate-500/50')
-              }
-              style={tw`text-slate-500 dark:text-slate-400`}
-              value={settingsStore.apiBaseUrl || ''}
-              onChangeText={(apiBaseUrl) => useSettingsStore.setState({ apiBaseUrl })}
-            />
-          )}
+          description={t('advanced.actions.switchTokensStorage.description')}
+          label={t('advanced.actions.switchTokensStorage.label')}
+          style={tw`px-3 mx-3`}>
+          <Switch
+            value={settingsStore.areTokensInAsyncStorage}
+            onColor={theme.meatBrown}
+            onValueChange={onSwitchAuthStorage}
+          />
+        </ServiceRow>
+        <ServiceRow
+          withBottomDivider
+          description={t('advanced.actions.clearCache.description')}
+          label={t('advanced.actions.clearCache.label')}
+          loading={isClearingCache}
           style={tw`px-3 mx-3`}
+          suffixIcon="trash-can-outline"
+          onPress={clearCache}
         />
         <ServiceRow
-          description={t('advanced.services.crash.description')}
-          label={t('advanced.services.crash.label')}
+          withBottomDivider
+          description={t('advanced.actions.crash.description')}
+          label={t('advanced.actions.crash.label')}
           style={tw`px-3 mx-3`}
           suffixIcon="bomb"
           onPress={() => {
             throw new Error("Don't worry, this is a test crash!");
           }}
         />
+        <ServiceRow
+          label={t('advanced.actions.reset.label')}
+          loading={isResetting}
+          style={tw`px-3 mx-3`}
+          suffixIcon="nuke"
+          onPress={confirmReset}
+        />
 
-        <AppText style={tw`text-sm uppercase text-slate-500 mx-6 mt-6`}>
-          {t('advanced.store.title')}
-        </AppText>
+        <SectionTitle style={tw`mx-6 mt-6`} title={t('advanced.variables.title')} />
+
         <ServiceRow
           withBottomDivider
-          label={t('advanced.store.onboarding.label')}
+          label={t('advanced.variables.onboarding.label')}
           style={tw`px-3 mx-3`}>
           <Switch
             value={settingsStore.hasOnboard}
@@ -202,7 +210,7 @@ const Advanced = () => {
         </ServiceRow>
         <ServiceRow
           withBottomDivider
-          label={t('advanced.store.hasLearnPullToRefresh.label')}
+          label={t('advanced.variables.hasLearnPullToRefresh.label')}
           style={tw`px-3 mx-3`}>
           <Switch
             value={settingsStore.hasLearnPullToRefresh}
@@ -212,7 +220,7 @@ const Advanced = () => {
         </ServiceRow>
         <ServiceRow
           withBottomDivider
-          label={t('advanced.store.withNativePullToRefresh.label')}
+          label={t('advanced.variables.withNativePullToRefresh.label')}
           style={tw`px-3 mx-3`}>
           <Switch
             value={settingsStore.withNativePullToRefresh}
@@ -222,7 +230,7 @@ const Advanced = () => {
         </ServiceRow>
         <ServiceRow
           withBottomDivider
-          label={t('advanced.store.hasSeenBirthdayPresentAt.label')}
+          label={t('advanced.variables.hasSeenBirthdayPresentAt.label')}
           style={tw`px-3 mx-3`}>
           <Switch
             value={!isNil(settingsStore.hasSeenBirthdayPresentAt)}
@@ -236,7 +244,7 @@ const Advanced = () => {
         </ServiceRow>
         <ServiceRow
           withBottomDivider
-          label={t('advanced.store.hasBeenInvitedToReview.label')}
+          label={t('advanced.variables.hasBeenInvitedToReview.label')}
           style={tw`px-3 mx-3`}>
           <Switch
             value={settingsStore.hasBeenInvitedToReview}
@@ -244,74 +252,36 @@ const Advanced = () => {
             onValueChange={(value) => useSettingsStore.setState({ hasBeenInvitedToReview: value })}
           />
         </ServiceRow>
-        <ServiceRow
-          withBottomDivider
-          description={`${authStore.accessToken}`}
-          label={t('advanced.store.accessToken.label')}
-          renderDescription={() => (
-            <TextInput
-              autoCapitalize="none"
-              keyboardType="default"
-              placeholder={isNil(authStore.accessToken) ? `${authStore.accessToken}` : ''}
-              placeholderTextColor={
-                tw.prefixMatch('dark') ? tw.color('slate-400/50') : tw.color('slate-500/50')
-              }
-              style={tw`text-slate-500 dark:text-slate-400`}
-              value={authStore.accessToken || ''}
-              onChangeText={(accessToken) =>
-                useAuthStore.setState({ accessToken: accessToken || null })
-              }
-            />
-          )}
-          style={tw`px-3 mx-3`}
+        <AppTextField
+          autoCapitalize="none"
+          containerStyle={tw`mt-6 mx-6`}
+          keyboardType="url"
+          label={t('advanced.variables.apiBaseUrl.label')}
+          placeholder={HTTP.defaults.baseURL}
+          value={settingsStore.apiBaseUrl || ''}
+          onChangeText={(apiBaseUrl) => useSettingsStore.setState({ apiBaseUrl })}
         />
-        <ServiceRow
-          withBottomDivider
-          description={`${authStore.refreshToken}`}
-          label={t('advanced.store.refreshToken.label')}
-          renderDescription={() => (
-            <TextInput
-              autoCapitalize="none"
-              keyboardType="default"
-              placeholder={isNil(authStore.refreshToken) ? `${authStore.refreshToken}` : ''}
-              placeholderTextColor={
-                tw.prefixMatch('dark') ? tw.color('slate-400/50') : tw.color('slate-500/50')
-              }
-              style={tw`text-slate-500 dark:text-slate-400`}
-              value={authStore.refreshToken || ''}
-              onChangeText={(refreshToken) =>
-                useAuthStore.setState({ refreshToken: refreshToken || null })
-              }
-            />
-          )}
-          style={tw`px-3 mx-3`}
+        <AppTextField
+          autoCapitalize="none"
+          containerStyle={tw`mx-6`}
+          keyboardType="default"
+          label={t('advanced.variables.accessToken.label')}
+          placeholder={isNil(authStore.accessToken) ? `${authStore.accessToken}` : ''}
+          value={`${authStore.accessToken}`}
+          onChangeText={(accessToken) =>
+            useAuthStore.setState({ accessToken: accessToken || null })
+          }
         />
-        <ServiceRow
-          withBottomDivider
-          description={t('advanced.store.tokensStorage.description')}
-          label={t('advanced.store.tokensStorage.label')}
-          style={tw`px-3 mx-3`}>
-          <Switch
-            value={settingsStore.areTokensInAsyncStorage}
-            onColor={theme.meatBrown}
-            onValueChange={onSwitchAuthStorage}
-          />
-        </ServiceRow>
-        <ServiceRow
-          withBottomDivider
-          description={t('advanced.store.clearCache.description')}
-          label={t('advanced.store.clearCache.label')}
-          loading={isClearingCache}
-          style={tw`px-3 mx-3`}
-          suffixIcon="trash-can-outline"
-          onPress={clearCache}
-        />
-        <ServiceRow
-          label={t('advanced.store.reset.label')}
-          loading={isResetting}
-          style={tw`px-3 mx-3`}
-          suffixIcon="nuke"
-          onPress={confirmReset}
+        <AppTextField
+          autoCapitalize="none"
+          containerStyle={tw`mx-6`}
+          keyboardType="default"
+          label={t('advanced.variables.refreshToken.label')}
+          placeholder={isNil(authStore.refreshToken) ? `${authStore.refreshToken}` : ''}
+          value={`${authStore.refreshToken}`}
+          onChangeText={(refreshToken) =>
+            useAuthStore.setState({ refreshToken: refreshToken || null })
+          }
         />
       </View>
     </ServiceLayout>
