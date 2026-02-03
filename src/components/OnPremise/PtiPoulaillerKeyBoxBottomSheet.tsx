e5bd889247e8b4e9789593f8711b334235a657d6
Matthieu Deleys <p.matthieu@me.com>
2026-02-03 20:26:24 +0100

    fix(theme): prefer neutral text color over slate in dark theme

--- src/components/OnPremise/PtiPoulaillerKeyBoxBottomSheet.tsx
+++ src/components/OnPremise/PtiPoulaillerKeyBoxBottomSheet.tsx
@@ -91,7 +91,8 @@ const PtiPoulaillerKeyBoxBottomSheet = ({
             size={24}
             style={tw`shrink-0 grow-0`}
           />
-          <AppText style={tw`text-base font-normal text-slate-500 shrink grow basis-0`}>
+          <AppText
+            style={tw`text-base font-normal text-slate-500 dark:text-neutral-500 shrink grow basis-0`}>
             {t('onPremise.keyBoxes.missingCapability')}
           </AppText>
         </View>
